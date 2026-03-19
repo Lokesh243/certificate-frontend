54dbd1d67b4203cf04513e3068dc5d9e90f06ebd
Lokesh <gajangilokesh@gmail.com>
2026-03-19 18:55:22 +0530

    production issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
-
-function Admin() {
-  return <h2>Admin Dashboard</h2>;
-}
-
-function Student() {
-  return <h2>Student Dashboard</h2>;
-}
+import AdminDashboard from "./components/AdminDashboard";
+import StudentDashboard from "./components/StudentDashboard";
+import AdminHome from "./components/AdminHome";
+import StudentHome from "./components/StudentHome";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Login */}
         <Route path="/" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/student" element={<Student />} />
+
+        {/* Admin */}
+        <Route path="/admin-home" element={<AdminHome />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+
+        {/* Student */}
+        <Route path="/student-home" element={<StudentHome />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+
       </Routes>
     </BrowserRouter>
   );
